abe1636bd582ff42a3338bcc010a2408b62050dd
bryanpeele <bryan.peele@gmail.com>
2016-01-11 00:25:43 -0500

    plot 2 added

---
+++ plot2.R
@@ -0,0 +1,12 @@
+data <-read.csv("household_power_consumption.txt",sep=";")
+
+data <- subset(data,Date=="1/2/2007" | Date=="2/2/2007")
+
+GAP_kW = as.numeric(data$Global_active_power)
+combodate <- with(data, as.POSIXct(paste(Date, Time), format="%d/%m/%Y %H:%M:%S"))
+
+png("plot2.PNG")
+
+plot(GAP_kW~combodate,ylab="Global Active Power (kilowatts)",xlab="",type="l")
+
+dev.off()
